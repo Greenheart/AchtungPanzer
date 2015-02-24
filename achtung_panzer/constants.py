c800ca84b5102b1662ea7aca669f59645b42d634
Samuel Plumppu <greenheartmusic@gmail.com>
2015-02-24 11:59:02 +0100

    Pygame init code
    Made pygame init code. Opens a blank window.
    A small state machine created for keeping track of the game state.
    Added constants.py for constants that will be needed in most of the code.

---
+++ achtung_panzer/constants.py
@@ -0,0 +1,6 @@
+# Use this file to store constant variables for the game.
+# This will make it easier to find them :)
+# When you add new constants --> Place it in alpabetical order!
+
+CAPTION = 'Achtung Panzer'
+SCREENSIZE = (1000, 700)
--- achtung_panzer/controller.py
+++ achtung_panzer/controller.py
@@ -1,9 +1,42 @@
 from agent import Player
 from map import World
+import sys
+import pygame
+from pygame.locals import *
+from constants import *   # constants are CAPITALIZED
+
+# Game States
+S_MENU = 1
+S_ARENA = 2
+S_UPGRADES = 3
 
 class Controller():
     def __init__(self):
-        pass
+        self.state = S_MENU
+        
+        ## Init pygame
+        pygame.init()
+        self.screen = pygame.display.set_mode(SCREENSIZE)
+        pygame.display.set_caption(CAPTION)
 
     def run(self):
-        pass
+        while True:
+            if self.state == S_MENU:
+                for event in pygame.event.get():
+                    if event.type == QUIT:
+                        pygame.quit()
+                        sys.exit()
+                    if event.type == KEYDOWN:
+                        if event.key == K_ESCAPE:
+                            pygame.quit()
+                            sys.exit()
+
+            if self.state == S_ARENA:
+                pass
+
+            if self.state == S_UPGRADES:
+                pass
+
+
+                        
+
