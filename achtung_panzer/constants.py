fd0a25872846658ab28562c8b8252004e70546a7
Hugo Lundin <hugo.lundin@hotmail.com>
2015-02-27 17:12:53 +0100

    - Added graphics folder, containing menu-graphics (right now very temporary graphics) - An early menu-version with buttons have been implemented, buttons can be hovered but not clicked. - Added some new constants to constants.py - Added import of os, and SDL_VIDEO_CENTERED for centering the pygame-window. - Added clock and pygame.display.flip() to be able to run the menu. - Added loading of graphics in __init__, for now.

--- achtung_panzer/constants.py
+++ achtung_panzer/constants.py
@@ -2,5 +2,7 @@
 # This will make it easier to find them :)
 # When you add new constants --> Place it in alpabetical order!
 
+BTN_ACTIVE = 1
+BTN_INACTIVE = 0
 CAPTION = 'Achtung Panzer'
 SCREENSIZE = (1000, 700)
