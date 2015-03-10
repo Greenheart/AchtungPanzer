2450a9d37798ad86a5ddbdc2fd7ef7fc498ca69d
Hugo Lundin <hugo.lundin@hotmail.com>
2015-03-10 11:49:35 +0100

    Menu
    Implemented within a class. Buttons have their own class.

--- achtung_panzer/controller.py
+++ achtung_panzer/controller.py
@@ -5,6 +5,7 @@ from constants import *   # constants are CAPITALIZED
 import os
 from agent import Player
 from map import World
+from menu import Menu
 
 # Game States
 S_MENU = 1
@@ -19,31 +20,6 @@ class Controller():
         ## Centers game window, needs to be before pygame.init()
         os.environ['SDL_VIDEO_CENTERED'] = '1'
 
-        ## Init pygame
-        pygame.init()
-        self.screen = pygame.display.set_mode(SCREEN_SIZE)
-        pygame.display.set_caption(CAPTION)
-        self.clock = pygame.time.Clock()
-
-        ## Load a lot of sprites
-
-        self.background = pygame.image.load("images/menu/background.png")
-        self.background = pygame.transform.scale(self.background, (SCREEN_SIZE))
-
-        self.logo = pygame.image.load("images/menu/logo.png")
-        self.logo = pygame.transform.scale(self.logo, (int(540), int(150.3)))
-
-        self.about_description = pygame.image.load("images/menu/about_description.png")
-        self.about_description = pygame.transform.scale(self.about_description, (int(540), int(243.6)))
-
-        self.start_button = (pygame.image.load("images/menu/startbutton.png"), pygame.image.load("images/menu/startbutton_hover.png"))
-        self.start_button_state = BTN_INACTIVE
-
-        self.about_button = (pygame.image.load("images/menu/aboutbutton.png"), pygame.image.load("images/menu/aboutbutton_hover.png"))
-        self.about_button_state = BTN_INACTIVE
-
-        self.settings_button = (pygame.image.load("images/menu/settingsbutton.png"), pygame.image.load("images/menu/settingsbutton_hover.png"))
-        self.settings_button_state = BTN_INACTIVE
 
         self.state = S_MENU
         self.caption = CAPTION
@@ -66,113 +42,58 @@ class Controller():
         self.register_eventhandler(pygame.QUIT, self.quit)
         self.register_key(pygame.K_ESCAPE, pygame.KEYDOWN, self.quit)
 
+        self.menu = Menu(self)
+
 
     def run(self):
 
         while True:
 
+            for event in pygame.event.get():
+                # Handle generic events
+                for event_type, callback in self.events.iteritems():
+                    if event.type == event_type:
+                        callback(event)
+
+                # Handle keyboard events
+                for event_key, event_type in self.keymap.iterkeys():
+                    if event.type == event_type and event.key == event_key:
+                        self.keymap[(event_key, event_type)](event)
+
             if self.state == S_MENU:
-                for event in pygame.event.get():
-                    if event.type == QUIT:
-                        pygame.quit()
-                        sys.exit()
-                    if event.type == KEYDOWN:
-                        if event.key == K_ESCAPE:
-                            pygame.quit()
-                            sys.exit()
-                    if event.type == pygame.MOUSEMOTION:
-                        x, y = event.pos
-                        if ( x in range(351, 649)) and ( y in range(325, 395)):
-                            self.start_button_state = BTN_ACTIVE
-                        else:
-                            self.start_button_state = BTN_INACTIVE
-                        if ( x in range(351, 649)) and ( y in range(410, 480)):
-                            self.about_button_state = BTN_ACTIVE
-                        else:
-                            self.about_button_state = BTN_INACTIVE
-                        if ( x in range(351, 649)) and ( y in range(495, 565)):
-                            self.settings_button_state = BTN_ACTIVE
-                        else:
-                            self.settings_button_state = BTN_INACTIVE
-                    if event.type == pygame.MOUSEBUTTONDOWN:
-                        x, y = event.pos
-                        if ( x in range(351, 649)) and ( y in range(325, 395)):
-                            print("Start")
-                            self.state = S_GAME
-                            self.start_button_state = BTN_INACTIVE
-                        if ( x in range(351, 649)) and ( y in range(410, 480)):
-                            print("About")
-                            self.state = S_ABOUT
-                            self.about_button_state = BTN_INACTIVE
-                        if ( x in range(351, 649)) and ( y in range(495, 565)):
-                            print("Settings")
-                            self.state = S_SETTINGS
-
-
-                ## Blits background and logo
-                self.screen.blit(self.background, (0,0))
-                self.screen.blit(self.logo, (230,100))
-
-                ## Blits buttons and changes depending on state.
-                self.screen.blit(self.start_button[self.start_button_state], (351, 325))
-                self.screen.blit(self.about_button[self.about_button_state], (351, 410))
-                self.screen.blit(self.settings_button[self.settings_button_state], (351, 495))
-
-
-            
-
+                self.menu.draw()
 
             if self.state == S_GAME:
-
-                for event in pygame.event.get():
-                    for event_type, callback in self.events.iteritems():
-                        if event.type == event_type:
-                            callback(event)
-
-                    for event_key, event_type in self.keymap.iterkeys():
-                        if event.type == event_type and event.key == event_key:
-                            self.keymap[(event_key, event_type)](event)
-
                 self.map.draw()
 
                 for player in self.agents:
                     player.update_position()
                     player.draw()
 
-                pygame.display.flip()
-                self.clock.tick(self.fps)
-
             if self.state == S_UPGRADES:
                 pygame.quit()
                 sys.exit()
 
             if self.state == S_ABOUT:
-                for event in pygame.event.get():
-                    if event.type == QUIT:
-                        pygame.quit()
-                        sys.exit()
-                    if event.type == KEYDOWN:
-                        if event.key == K_ESCAPE:
-                            pygame.quit()
-                            sys.exit()
-                    if event.type == MOUSEBUTTONDOWN:
-                        self.state = S_MENU
-                self.screen.blit(self.background, (0,0))
-                self.screen.blit(self.logo, (230, 100))
-                self.screen.blit(self.about_description, (230, 325))
+                self.menu = Menu()
+                self.menu.about()
 
 
             if self.state == S_SETTINGS:
-                pygame.quit()
-                sys.exit()
-
+                self.menu = Menu()
+                self.menu.settings()
+                
             pygame.display.flip()
             self.clock.tick(60)
+
 
     def quit(self, event):
         pygame.quit()
         sys.exit()
-                        
+                   
+
+    def start_game(self):
+        self.state = S_GAME
 
     def register_key(self, event_key, event_type, callback):
         self.keymap[(event_key, event_type)] = callback
---
+++ achtung_panzer/menu.py
@@ -0,0 +1,114 @@
+import pygame
+from pygame.locals import *
+from constants import *
+import os, sys
+
+S_MENU_MAIN = 1
+S_MENU_ABOUT = 2
+S_MENU_SETTINGS = 3
+
+class Menu():
+    def __init__(self, controller):
+        self.screen = controller.screen
+        self.controller = controller
+        self.events = []
+
+        self.background = pygame.image.load("images/menu/background.png")
+        self.background = pygame.transform.scale(self.background, (SCREEN_SIZE))
+
+        self.logo = pygame.image.load("images/menu/logo.png")
+
+        self.about_description = pygame.image.load("images/menu/about_description.png")
+
+        self.buttons = []
+        self.buttons.append(Button(self, self.start_press, S_MENU_MAIN, (351, 325, 649, 395), "images/menu/startbutton.png", "images/menu/startbutton_hover.png"))
+        self.buttons.append(Button(self, self.about_press, S_MENU_MAIN, (351, 410, 649, 480), "images/menu/aboutbutton.png", "images/menu/aboutbutton_hover.png"))
+        self.buttons.append(Button(self, self.settings_press, S_MENU_MAIN, (351, 495, 649, 565), "images/menu/settingsbutton.png", "images/menu/settingsbutton_hover.png"))
+
+        self.buttons.append(Button(self, self.return_to_main, S_MENU_ABOUT, (351, 575, 649, 645), "images/menu/backbutton.png", "images/menu/backbutton_hover.png"))
+
+
+        self.state = S_MENU_MAIN
+
+
+        # Register event at controller
+        self.controller.register_eventhandler(pygame.MOUSEMOTION, self.mouse_event)
+        self.controller.register_eventhandler(pygame.MOUSEBUTTONDOWN, self.mouse_event)
+
+
+    def register_eventhandler(self, event_type, state, callback):
+        self.events.append((event_type, state, callback))
+
+
+    def mouse_event(self, event):
+        for event_type, state, callback in self.events:
+            if event.type == event_type and state == self.state:
+                callback(event)
+
+
+    def draw(self):
+        ## Blits background and logo
+        self.screen.blit(self.background, (0,0))
+        self.screen.blit(self.logo, (230,100))
+
+        for button in self.buttons:
+            button.draw()
+
+        if self.state == S_MENU_ABOUT:
+            self.screen.blit(self.logo, (230, 100))
+            self.screen.blit(self.about_description, (230, 325))
+
+    def start_press(self, event):
+        self.controller.start_game()
+
+    def about_press(self, event):
+        self.state = S_MENU_ABOUT
+
+    def settings_press(self, event):
+        pass
+
+    def return_to_main(self, event):
+        self.state = S_MENU_MAIN
+
+
+    def settings():
+        pygame.quit()
+        sys.exit()
+
+
+class Button():
+    def __init__(self, menu, callback, active_state, coords, image_normal, image_hover):
+        self.menu = menu
+        self.screen = menu.screen
+        self.callback = callback
+        self.active_state = active_state
+        self.x1, self.y1, self.x2, self.y2 = coords
+
+        self.hover = False
+        self.pressed = False
+
+        self.image_normal = pygame.image.load(image_normal)
+        self.image_hover = pygame.image.load(image_hover)
+
+        self.menu.register_eventhandler(pygame.MOUSEMOTION, self.active_state, self.mousemotion)
+        self.menu.register_eventhandler(pygame.MOUSEBUTTONDOWN, self.active_state, self.mousebuttondown)
+
+    def mousemotion(self, event):
+        x, y = event.pos
+        if self.menu.state == self.active_state and x >= self.x1 and x < self.x2 and y >= self.y1 and y < self.y2:
+            self.hover = True
+        else:
+            self.hover = False
+
+    def mousebuttondown(self, event):
+        x, y = event.pos
+        if self.menu.state == self.active_state and x >= self.x1 and x < self.x2 and y >= self.y1 and y < self.y2:
+            self.pressed = True
+            self.callback(event)
+        else:
+            self.pressed = False
+
+    def draw(self):
+        if self.menu.state == self.active_state:
+            image = self.image_hover if self.hover else self.image_normal
+            self.screen.blit(image, (self.x1, self.y1))
