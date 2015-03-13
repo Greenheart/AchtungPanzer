152e23d506d6f32709d7c862153b2fb5c4b46bc2
Hugo Lundin <hugo.lundin@hotmail.com>
2015-03-13 22:06:50 +0100

    Revert "Revert "Made a temporary bullet shooting function in Player class, will probably be replaced since we will have different types of bullets wich is not supported at the moment.""
    This reverts commit 5274bb14d55e9609c914fc6687cf786ef04c6653.

--- achtung_panzer/constants.py
+++ achtung_panzer/constants.py
@@ -2,11 +2,14 @@
 This file contains all the constant variables
 When adding variables, make sure to place them in an alphabetical order
 '''
+BULLET_SIZE = 5
+BULLET_SPEED = 5
 CAPTION = "Achtung Panzer"
 FPS = 60
+HEALTHBAR_SIZE = (0.5, 5)
 SCREEN_SIZE = (1000, 700)
 TANK_SPEED = 3
 TANK_ACCELERATION = 0.125
 TANK_ROTATION_SPEED = 2
-TANK_HEIGHT = 50
-TANK_WIDTH = 50
+TANK_HEIGHT = 5
+TANK_WIDTH = 500
