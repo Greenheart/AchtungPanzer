11b51f7761eae16b6cd7f7fd6d9ed4e39ae1ae1e
Hugo Lundin <hugo.lundin@hotmail.com>
2015-03-17 13:03:09 +0100

    Added some ugly dead trees to sand-map.

--- achtung_panzer/map.py
+++ achtung_panzer/map.py
@@ -14,7 +14,11 @@ class World():
 
         elif map_type == "sand":
             self.ground_sprite = pygame.image.load("images/sand.png")
-            self.objects.append((pygame.image.load("images/busksten.png"), (random.randint(0, SCREEN_SIZE[0]), random.randint(0, SCREEN_SIZE[1]))))
+            self.objects.append((pygame.image.load("images/deadtree.png"), (random.randint(0, SCREEN_SIZE[0]), random.randint(0, SCREEN_SIZE[1]))))
+            self.objects.append((pygame.image.load("images/deadtree.png"), (random.randint(0, SCREEN_SIZE[0]), random.randint(0, SCREEN_SIZE[1]))))
+            self.objects.append((pygame.image.load("images/deadtree.png"), (random.randint(0, SCREEN_SIZE[0]), random.randint(0, SCREEN_SIZE[1]))))
+            self.objects.append((pygame.image.load("images/deadtree.png"), (random.randint(0, SCREEN_SIZE[0]), random.randint(0, SCREEN_SIZE[1]))))
+
 
         else: #Add more map_types here
         	pass
