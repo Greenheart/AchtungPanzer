29d94ab1aa47b53c05ae2b216ddf65a095dcb60b
Szymon Stypa <shabo.o@hotmail.com>
2015-05-05 09:59:28 +0200

    Fixed some bugs from last commit

--- achtung_panzer/functions.py
+++ achtung_panzer/functions.py
@@ -5,7 +5,7 @@ def detect_collision(obj1, obj2):
 	if obj1.type == 1: #If the object is an area object
 		for circle in obj1.circles:
 			minimal_distance = circle.radius + obj2.radius
-			distance = math.sqrt((math.fabs(float(circle.x - obj2.x)))**2 + (math.fabs(float(circle.y - obj2.y)))**2 )
+			current_distance = math.sqrt((math.fabs(float(circle.x - obj2.x)))**2 + (math.fabs(float(circle.y - obj2.y)))**2 )
 
 			if current_distance <= minimal_distance:
 				break
@@ -13,14 +13,14 @@ def detect_collision(obj1, obj2):
 	elif obj2.type == 1: #If the object is an area object
 		for circle in obj2.circles:
 			minimal_distance = circle.radius + obj1.radius
-			distance = math.sqrt((math.fabs(float(circle.x - obj1.x)))**2 + (math.fabs(float(circle.y - obj1.y)))**2 )
+			current_distance = math.sqrt((math.fabs(float(circle.x - obj1.x)))**2 + (math.fabs(float(circle.y - obj1.y)))**2 )
 
 			if current_distance <= minimal_distance:
 				break
 
 	else:
 		minimal_distance = obj1.radius + obj2.radius
-		distance = math.sqrt((math.fabs(float(obj1.x - obj2.x)))**2 + (math.fabs(float(obj1.y - obj2.y)))**2 )
+		current_distance = math.sqrt((math.fabs(float(obj1.x - obj2.x)))**2 + (math.fabs(float(obj1.y - obj2.y)))**2 )
 
 	if current_distance <= minimal_distance:
 		return True
