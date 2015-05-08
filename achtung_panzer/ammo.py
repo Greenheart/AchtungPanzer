1036213a0000a3f9f148f643e3505d4d333686ad
Samuel Plumppu <greenheartmusic@gmail.com>
2015-05-08 22:52:55 +0200

    Improved collision between Ammo-objects and players.

--- achtung_panzer/ammo.py
+++ achtung_panzer/ammo.py
@@ -1,15 +1,19 @@
 import pygame
 import math
+from functions import *
 from constants import *
 from animation import *
 
 
 class Ammo(object):
 
-	def __init__(self, player):
+	def __init__(self, player, sprite):
 
+		self.type = 0
+		self.name = "Ammo"
 		self.speed = 1
 		self.damage = 1
+		self.solid = 0
 
 		self.x = 1
 		self.y = 1
@@ -18,16 +22,25 @@ class Ammo(object):
 
 		self.width, self.height = 1, 1
 
-		self.sprite = pygame.image.load("images/ammo/default.png")
+		self.sprite = sprite
+		self.width = sprite.get_width() 
+		self.height = sprite.get_height()
 
 		self.player = player
 		self.controller = self.player.controller
+		if self.width > self.height:
+			self.radius = self.width / 2
+		else:
+			self.radius = self.height / 2
 
 	def update(self):
 		pass
 
 	def draw(self):
-		self.player.screen.blit(self.sprite, (self.x, self.y))
+		self.player.screen.blit(self.sprite, (self.x - self.width/2, self.y-self.height/2))
+		
+		if self.controller.debug:
+			pygame.draw.circle(self.controller.screen, (255,0,0), (int(self.x), int(self.y)), self.radius, 2)
 
 
 """-------------------------------------------------AMMO TYPES-----------------------------------------------------------"""
@@ -37,7 +50,7 @@ class Bullet(Ammo):
 
 	def __init__(self, player, speed, damage, width, height, sprite):
 
-		super(Bullet, self).__init__(player)
+		super(Bullet, self).__init__(player, sprite)
 
 		self.x, self.y = player.x, player.y
 
@@ -59,7 +72,8 @@ class Bullet(Ammo):
 
 		for player in self.controller.agents:
 			if player != self.player:
-				if self.x > player.x - player.sprite.get_width()/2 and self.x < player.x + player.sprite.get_width()/2 and self.y > player.y - player.sprite.get_height()/2 and self.y < player.y + player.sprite.get_height()/2:
+
+				if detect_collision(self, player):
 					self.controller.ammo.remove(self)
 					player.health -= self.damage
 					Animation(self.player.screen, "explosion", (self.x, self.y), 4)
@@ -85,6 +99,7 @@ class NormalShot(Bullet):
 		super(NormalShot, self).__init__(player, speed, damage, width, height, sprite)
 
 		self.sprite.set_alpha(200)
+		self.radius = 5
 
 
 class Mine(Bullet):
